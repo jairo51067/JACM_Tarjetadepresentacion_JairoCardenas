fdacd19a7efc8c9f856eea7b2bbc7bc377f2e3ba
jairo51067 <jairocardenas05@gmail.com>
2023-11-09 17:59:38 -0400

    Se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,7 @@
+// window.alert("'Hola esta es una tarjeta de presentacion de Jairo Cárdenas'");
+
+function mySkills() {
+    document.getElementById("skill").innerHTML = "- HTML - CCS - JavaScript - ";
+  }
+
+  

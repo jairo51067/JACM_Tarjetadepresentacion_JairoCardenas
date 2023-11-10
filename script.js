edc43a36045cc89061d4a4c99338d4447752a064
jairo51067 <jairocardenas05@gmail.com>
2023-11-10 11:38:30 -0400

    Elimino imprimir la pagina y agrego funcionalida al boton skills

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 // window.alert("'Hola esta es una tarjeta de presentacion de Jairo Cárdenas'");
 
-function mySkills() {
-    document.getElementById("skill").innerHTML = "- HTML - CCS - JavaScript - ";
-  }
 
-  
+function mySkills() {
+
+  document.getElementById("skill").innerHTML = "Web Designer";
+
+  const elemento = document.getElementById('skill');
+
+  const contenidoOriginal = elemento.innerHTML;
+
+  elemento.innerHTML = " - HTML - CCS - JavaScript - "
+
+  setTimeout(()=>{
+    elemento.innerHTML = contenidoOriginal;
+}, 2500);
+
+}

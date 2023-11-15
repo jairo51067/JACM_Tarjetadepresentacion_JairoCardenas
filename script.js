34a82b29a9ec145414a489f6164d0da120deb21a
jairo51067 <jairocardenas05@gmail.com>
2023-11-15 17:05:01 -0400

    se agrega nueva funcion del icono imprimir CV, se modifica el texto de about me, se adiciona el CV original

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+/*Utilizado para mostrar un ventana de alerta al cargar la pagina*/
+
+// window.alert("'Hola esta es una tarjeta de presentacion de Jairo Cárdenas'");
+
+
+/*Codigo utilizado para que el titilo se lea y vuelva al origian en el tiempo elegido*/
+
+// function mySkills() {
+
+//   document.getElementById("skill").innerHTML = "Web Designer";
+
+//   const elemento = document.getElementById('skill');
+
+//   const contenidoOriginal = elemento.innerHTML;
+
+//   elemento.innerHTML = " - HTML - CCS - JavaScript - "
+
+//   setTimeout(()=>{
+//     elemento.innerHTML = contenidoOriginal;
+// }, 2500);
+
+// }
+
+
+/*Codigo para cambiar el texto al hacer click en el boton*/
+
+// function mySkills() {
+//   document.getElementById("skill").innerHTML = " - HTML - CCS - JavaScript - ";
+
+// }
+
+/* */
+
+
 let hideText_btn = document.getElementById('hideText_btn');
 
 let hideText = document.getElementById('hideText');
@@ -14,3 +48,10 @@ function toggleText(){
     hideText_btn.innerHTML = 'skills';
   }
 }
+
+
+
+function mi_CV(){
+  window.alert("Hola este CV esta actualizado al año 2023");
+}
+
